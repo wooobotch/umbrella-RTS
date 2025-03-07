e5bdd80c27fdcc5a4ba92aae96336bdc8afcb101
Abraham Ezequiel Chalave <abraham.chalave@outlook.com>
2025-03-06 23:25:27 -0300

    CMakeLists typos

--- setup.sh
+++ setup.sh
@@ -5,5 +5,5 @@ sudo apt update && sudo apt install -y \
     libglfw3-dev libglfw3 \
     libglew-dev libglm-dev \
     libxrandr-dev libxinerama-dev libxcursor-dev libxi-dev \
-    mesa-utils
+    mesa-utils libsdl2-dev
 echo "Entorno configurado correctamente."
